9db92e8457f3e910fad6b3efd2138b978771a60e
Daniel Rizal <flipinteract@gmail.com>
2014-03-24 12:56:43 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pintersting::Application.config.secret_key_base = 'b98208bdfe5cbf9d9a077e03d4db9ed8bc2711a6463ca635a11e253fe457775f2d0280fd1a158f1488d4a9bfcfaae8e003cfa6edb45c61d2121ac4bdced591b3'
